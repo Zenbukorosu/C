2d8becd0ecf85d9d0b4f9241c93942b62c313eec
Zenbukorosu <303613070@qq.com>
2023-01-26 19:59:06 +0800

    练习

---
+++ 2.12-6.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+	int toes;
+	toes = 10;
+	printf("toes=%d\n",toes);
+	printf("toes*2=%d\n",toes*2);
+	printf("toes^2=%d",toes*toes);//计算机不会将^识别为平方运算
+	return 0;
+}
---
+++ 2.12-7.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+void smi(void);
+int main()
+{
+	smi();
+	smi();
+	printf("Smile!\n");
+	smi();
+	printf("Smile!\n");
+	smi();
+	return 0;
+}
+void smi(void)
+{
+	printf("Smile!");
+}
