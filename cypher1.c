8f01c14e22514e19f234ddeaa5964832184e1d7c
Zenbukorosu <303613070@qq.com>
2023-02-10 23:08:56 +0800

    练习

---
+++ cypher1.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#define SPACE ' ' // SPACE表示单引号-空格-单引号
+int main(void)
+{
+    char ch;
+    while ((ch = getchar()) != '\n')
+    {
+        if (ch == SPACE)
+            putchar(ch);
+        else
+            putchar(ch + 1);
+    };
+
+    return 0;
+}
