43dcbbeb3b4eae1a14cd7e74752fb8635ab03926
Zenbukorosu <303613070@qq.com>
2023-02-11 22:47:55 +0800

    练习

--- divisors.c
+++ divisors.c
@@ -1,25 +1,25 @@
 #include <stdio.h>
+#include <stdbool.h>
 int main(void)
 {
     unsigned long num, div;
-    int isPrime = 1;
+    bool isPrime;
 
     printf("Please enter an integer for analysis(q to stop):\n");
-    while ((scanf("%lu", &num)) == 1)
+    while (scanf("%lu", &num) == 1)
     {
-        for (int div = 2; (div * div) <= num; div++)
+        for (div = 2, isPrime = true; (div * div) <= num; div++)
         {
             if (num % div == 0) // 判断整除
             {
-                isPrime = 1;
                 if ((div * div) == num)
                     printf("%lu is devided by %ld.\n", num, div);
                 else
                     printf("%lu is devided by %ld and %d.\n", num, div, num / div);
-                isPrime = 0; // 用来判断是否流入循环
+                isPrime = false; // 用来判断是否流入循环
             };
         };
-        if (isPrime == 1)
+        if (isPrime)
             printf("%lu is a prime.\n", num);
         printf("Please enter next integer to analysis(q to quit):\n");
     };
